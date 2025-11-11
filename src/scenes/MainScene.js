e69de22394ae8d9429798b4bfcb30bb58bbe694d
Derek Buckley <dereksbuckley@gmail.com>
2025-11-11 07:44:04 -0500

    changed mega jackpot

--- src/scenes/MainScene.js
+++ src/scenes/MainScene.js
@@ -1316,7 +1316,7 @@ export default class MainScene extends Phaser.Scene {
       // Mark as used in database
       this.firstTimeEvents.guaranteed_mega_jackpot = true;
       console.log('First-time mega jackpot flag set to true');
-    } else if (rand < 0.007) {
+    } else if (rand < 0.006) {
       // 0.8% mega jackpot (normal probability)
       isMegaJackpot = true;
       coinReward = Phaser.Utils.Array.GetRandom([3000, 4000, 5000]);
